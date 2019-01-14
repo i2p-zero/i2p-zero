55a4bb387517bbf12914bcef6065f350d3c75bd9
knaccc <knaccc@users.noreply.github.com>
2019-01-14 19:22:49 +0000

    fixed echo

--- bin/convert-jars-to-modules.sh
+++ bin/convert-jars-to-modules.sh
@@ -9,12 +9,12 @@ rm -f $basedir/target/modules/*
 
 for jarPath in $jarPaths; do
   moduleName=$(basename "${jarPath%.*}")
-  echo "*** Determining dependencies for " $moduleName
+  echo "*** Determining dependencies for $moduleName"
   jdeps --module-path $basedir/import/lib --add-modules=ALL-MODULE-PATH --generate-module-info $basedir/target/module-info $jarPath
 done
 for jarPath in $jarPaths; do
   moduleName=$(basename "${jarPath%.*}")
-  echo "*** Creating new modular jar for " $moduleName
+  echo "*** Creating new modular jar for $moduleName"
   javac --module-path $basedir/import/lib --patch-module $moduleName=$jarPath $basedir/target/module-info/$moduleName/module-info.java
   cp $jarPath $basedir/target/modules/
   jar uf $basedir/target/modules/${moduleName}.jar -C $basedir/target/module-info/$moduleName module-info.class
