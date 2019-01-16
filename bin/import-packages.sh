83c7af515a28bc9077010279c64c86f938967e70
knaccc <knaccc@users.noreply.github.com>
2019-01-16 21:59:56 +0000

    Clean up import dirs if already exist

--- bin/import-packages.sh
+++ bin/import-packages.sh
@@ -7,13 +7,13 @@ source $basedir/bin/java-config.sh
 mkdir -p $basedir/import
 cd $basedir/import
 
-if [ ! -d "$basedir/i2p.i2p" ]; then
+if [ ! -d "$basedir/import/i2p.i2p" ]; then
   git clone https://github.com/i2p/i2p.i2p.git
 fi
 
 git checkout tags/i2p-0.9.37
 
-if [ ! -d "$basedir/jdks" ]; then
+if [ ! -d "$basedir/import/jdks" ]; then
   mkdir -p jdks
   mkdir -p jdks/linux jdks/mac jdks/win
   wget --directory-prefix=jdks/linux $JDK_DOWNLOAD_URL_LINUX
@@ -25,7 +25,7 @@ if [ ! -d "$basedir/jdks" ]; then
   unzip jdks/win/$JDK_DOWNLOAD_FILENAME_WIN -d jdks/win/
 fi
 
-if [ ! -d "$basedir/apache-ant-1.10.5" ]; then
+if [ ! -d "$basedir/import/apache-ant-1.10.5" ]; then
   wget https://www-us.apache.org/dist/ant/binaries/apache-ant-1.10.5-bin.tar.gz
   tar zxvf apache-ant-1.10.5-bin.tar.gz
 fi
