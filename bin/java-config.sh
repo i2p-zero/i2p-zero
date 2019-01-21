3aa8b6c383d26e5c80a9ae5269cdcf3d75971644
knaccc <knaccc@users.noreply.github.com>
2019-01-21 05:24:07 +0000

    switched to Github hosted github.com/AdoptOpenJDK site for OpenJDK

--- bin/java-config.sh
+++ bin/java-config.sh
@@ -10,9 +10,9 @@ JDK_DOWNLOAD_URL_LINUX=https://github.com/AdoptOpenJDK/openjdk11-binaries/releas
 JDK_DOWNLOAD_URL_MAC=https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.1%2B13/OpenJDK11U-jdk_x64_mac_hotspot_11.0.1_13.tar.gz
 JDK_DOWNLOAD_URL_WIN=https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.1%2B13/OpenJDK11U-jdk_x64_windows_hotspot_11.0.1_13.zip
 
-JAVA_HOME_LINUX=`realpath $basedir/import/jdks/linux/jdk-11.0.1+13`
-JAVA_HOME_MAC=`realpath $basedir/import/jdks/mac/jdk-11.0.1+13/Contents/Home`
-JAVA_HOME_WIN=`realpath $basedir/import/jdks/win/jdk-11.0.1+13`
+JAVA_HOME_LINUX=`$basedir/import/jdks/linux/jdk-11.0.1+13`
+JAVA_HOME_MAC=`$basedir/import/jdks/mac/jdk-11.0.1+13/Contents/Home`
+JAVA_HOME_WIN=`$basedir/import/jdks/win/jdk-11.0.1+13`
 
 OS=`uname -s`
 if [ $OS = "Darwin" ]; then
