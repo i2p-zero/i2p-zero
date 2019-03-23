8df76c4fe0582923248b5b577ce1c9377c37e7c0
knaccc <knaccc@users.noreply.github.com>
2019-03-23 17:23:32 +0000

    automatically use VERSION in zip-all.sh script

--- bin/zip-all.sh
+++ bin/zip-all.sh
@@ -6,18 +6,13 @@ else
     basedir=$(dirname $(dirname $(readlink -fm $0)))
 fi
 
-if [ $# -eq 0 ]; then
-  echo "Specify a version number for the release zip"
-  exit
-fi
-
 rm -fr "$basedir/dist-zip"
 mkdir -p "$basedir/dist-zip"
 
-VERSION=$1
+VERSION=$(head -n 1 "$basedir/org.getmonero.i2p.zero/src/org/getmonero/i2p/zero/VERSION")
 
 cd "$basedir/dist"
-for i in linux linux-gui mac mac-gui win win-gui; do zip -r9 "$basedir"/dist-zip/${i}.${VERSION}.zip ${i}; done
+for i in linux linux-gui mac mac-gui win win-gui; do zip -r9 "$basedir"/dist-zip/${i}.v${VERSION}.zip ${i}; done
 cd ..
 
 du -sh  "$basedir/dist-zip/"*.zip
