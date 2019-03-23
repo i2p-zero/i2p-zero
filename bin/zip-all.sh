5c1e9784063a9e705b532f61b534be3cc998f031
knaccc <knaccc@users.noreply.github.com>
2019-03-23 19:46:01 +0000

    build tar.bz2 files instead of zip files for linux/mac

--- bin/zip-all.sh
+++ bin/zip-all.sh
@@ -6,13 +6,23 @@ else
     basedir=$(dirname $(dirname $(readlink -fm $0)))
 fi
 
+rm -fr "$basedir/dist-zip-staging"
+mkdir -p "$basedir/dist-zip-staging"
+
 rm -fr "$basedir/dist-zip"
 mkdir -p "$basedir/dist-zip"
 
 VERSION=$(head -n 1 "$basedir/org.getmonero.i2p.zero/src/org/getmonero/i2p/zero/VERSION")
 
 cd "$basedir/dist"
-for i in linux linux-gui mac mac-gui win win-gui; do zip -r9 "$basedir"/dist-zip/${i}.v${VERSION}.zip ${i}; done
+
+for i in linux linux-gui mac mac-gui win win-gui; do cp -r ${i} "$basedir"/dist-zip-staging/i2p-zero-${i}.v${VERSION}; done
+
+cd "$basedir/dist-zip-staging"
+
+for i in win win-gui; do zip -r9 "$basedir"/dist-zip/i2p-zero-${i}.v${VERSION}.zip i2p-zero-${i}.v${VERSION}; done
+for i in linux linux-gui mac mac-gui; do tar -jcvf "$basedir"/dist-zip/i2p-zero-${i}.v${VERSION}.tar.bz2 i2p-zero-${i}.v${VERSION}; done
 cd ..
 
 du -sh  "$basedir/dist-zip/"*.zip
+du -sh  "$basedir/dist-zip/"*.tar.bz2
