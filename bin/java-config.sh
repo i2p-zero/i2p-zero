f79ed54a609a4785cee156c08be5761d8c2609a1
knaccc <knaccc@users.noreply.github.com>
2020-02-25 20:47:58 +0000

    updated to latest AdoptOpenJDK jdk-13.0.2+8

--- bin/java-config.sh
+++ bin/java-config.sh
@@ -12,13 +12,13 @@ JDK_DOWNLOAD_FILENAME_LINUX=OpenJDK13U-jdk_x64_linux_hotspot_13_33.tar.gz
 JDK_DOWNLOAD_FILENAME_MAC=OpenJDK13U-jdk_x64_mac_hotspot_13_33.tar.gz
 JDK_DOWNLOAD_FILENAME_WIN=OpenJDK13U-jdk_x64_windows_hotspot_13_33.zip
 
-JDK_DOWNLOAD_URL_LINUX=https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13%2B33/OpenJDK13U-jdk_x64_linux_hotspot_13_33.tar.gz
-JDK_DOWNLOAD_URL_MAC=https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13%2B33/OpenJDK13U-jdk_x64_mac_hotspot_13_33.tar.gz
-JDK_DOWNLOAD_URL_WIN=https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13%2B33/OpenJDK13U-jdk_x64_windows_hotspot_13_33.zip
+JDK_DOWNLOAD_URL_LINUX=https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13.0.2%2B8/OpenJDK13U-jdk_x64_linux_hotspot_13.0.2_8.tar.gz
+JDK_DOWNLOAD_URL_MAC=https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13.0.2%2B8/OpenJDK13U-jdk_x64_mac_hotspot_13_33.tar.gz
+JDK_DOWNLOAD_URL_WIN=https://github.com/AdoptOpenJDK/openjdk13-binaries/releases/download/jdk-13.0.2%2B8/OpenJDK13U-jdk_x64_windows_hotspot_13_33.zip
 
-JAVA_HOME_LINUX=$basedir/import/jdks/linux/jdk-13+33
-JAVA_HOME_MAC=$basedir/import/jdks/mac/jdk-13+33/Contents/Home
-JAVA_HOME_WIN=$basedir/import/jdks/win/jdk-13+33
+JAVA_HOME_LINUX=$basedir/import/jdks/linux/jdk-13.0.2+8
+JAVA_HOME_MAC=$basedir/import/jdks/mac/jdk-13.0.2+8/Contents/Home
+JAVA_HOME_WIN=$basedir/import/jdks/win/jdk-13.0.2+8
 
 JAVAFX_VERSION=13
 JAVAFX_SDK_DOWNLOAD_URL_LINUX=https://download2.gluonhq.com/openjfx/$JAVAFX_VERSION/openjfx-${JAVAFX_VERSION}_linux-x64_bin-sdk.zip
